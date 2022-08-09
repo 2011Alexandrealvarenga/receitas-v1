1c25d24a40e788c0213c46f56684cd1e6e34f014
2011 Alexandre <2011brunofernandes@gmail.com>
2022-08-09 15:32:28 -0300

    seguranca - verificar se tem a funcao

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php 
+/*
+Plugin Name: Receitas
+Plugin URI:
+Description: 
+Version: 5.12.3
+Author: 
+Author URI:
+Text Domain: 
+Domain Path: /lang
+*/
+
+// recurso de segurança
+if(!function_exists('add_action')){
+    echo 'Opa! eu sou só um plugin, não posso ser chamado diretamente';
+    exit;
+}
+// setup
+// includes
+// hooks
+// shortcodes
